a84bdfd72d94d21024c954da341b6f24079aaf74
[Peanut] <peeranat45@gmail.com>
2023-12-21 21:39:15 +0700

    change feature

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -200,7 +200,7 @@ exports.getCategoryEntry = catchAsync(async (req, res, next) => {
     },
   ]);
 
-  const mainAttribute = otherAttribute.shift();
+  const mainAttribute = otherAttribute[0];
   formatOutput["mainAttribute"] = mainAttribute.accessorKey;
   formatOutput["entryDefinitions"] = otherAttribute;
 
